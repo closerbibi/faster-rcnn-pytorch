e699b5143ea37cbdebdbae6972be369af2321679
closerbibi <closerbibi@gmail.com>
2017-11-15 19:11:14 +0800

    pascalvoc first commit

--- train.py
+++ train.py
@@ -39,13 +39,11 @@ def log_print(text, color=None, on_color=None, attrs=None):
 
 # hyper-parameters
 # ------------
-#imdb_name = 'voc_2007_trainval'
+imdb_name = 'voc_2007_trainval'
 logger = Logger('./tb-logs')
-imdb_name = 'inria_train'
 cfg_file = 'experiments/cfgs/faster_rcnn_end2end.yml'
 pretrained_model = 'data/pretrain_model/VGG_imagenet.npy'
-#pretrained_model = 'models/gupta_19classes/faster_rcnn_100000.h5'
-output_dir = 'models/resnet101-bn-block1-fix-sgd'
+output_dir = 'models/resnet101-pascalvoc'
 
 start_step = 0
 end_step = 160000
@@ -55,7 +53,6 @@ print 'iter : %d, step_size : %s, lr_decay : %f'%(end_step, lr_decay_steps, lr_d
 rand_seed = 1024
 _DEBUG = True
 use_tensorboard = True
-exp_name = None # the previous experiment name in TensorBoard
 
 # ------------
 
@@ -65,20 +62,16 @@ if rand_seed is not None:
 # load config
 cfg_from_file(cfg_file)
 lr = cfg.TRAIN.LEARNING_RATE
-#lr = 0.00000001
 momentum = cfg.TRAIN.MOMENTUM
 weight_decay = cfg.TRAIN.WEIGHT_DECAY
-#disp_interval = cfg.TRAIN.DISPLAY
-disp_interval = 100
+disp_interval = cfg.TRAIN.DISPLAY # reset to 50
 log_interval = cfg.TRAIN.LOG_IMAGE_ITERS
 
 # load data
 imdb = get_imdb(imdb_name)
 rdl_roidb.prepare_roidb(imdb)
 roidb = imdb.roidb
-#pdb.set_trace()
 data_layer = RoIDataLayer(roidb, imdb.num_classes)
-#pdb.set_trace()
 # load net
 net = FasterRCNN(classes=imdb.classes, debug=_DEBUG)
 network.weights_normal_init(net, dev=0.01)
@@ -90,28 +83,28 @@ network.weights_normal_init(net, dev=0.01)
 weight_path = {
     'resnet50coco': '/home/closerbibi/.torch/models/res50_faster_rcnn_iter_1190000.pth',
     'myres101': '/home/closerbibi/workspace/pytorch-repo/frcnn/frcnn-resnet/output/default/voc_2007_trainval/default/res101_faster_rcnn_iter_70000.pth',
-    'ruores101': '/home/closerbibi/.torch/models/res101_faster_rcnn_iter_110000.pth'
+    'ruores101': '/home/closerbibi/.torch/models/res101_faster_rcnn_iter_110000.pth',
+    'imageNet': '/home/closerbibi/.torch/models/resnet101-caffe.pth'
 }
-network.load_resnet_weight(net, weight_path['ruores101'])
-
-# model_file = '/media/longc/Data/models/VGGnet_fast_rcnn_iter_70000.h5'
-# model_file = 'models/saved_model3/faster_rcnn_60000.h5'
-# network.load_net(model_file, net)
-# exp_name = 'vgg16_02-19_13-24'
-# start_step = 60001
-# lr /= 10.
-# network.weights_normal_init(1[net.bbox_fc, net.score_fc, net.fc6, net.fc7], dev=0.01)
+network.load_resnet_weight(net, weight_path['imageNet'])
 
 net.cuda()
 net.train()
 
 params = list(net.parameters())
+
+#### Optimizer Choosing ######
+
+### Adam ###
 # adam doesn't need to update lr manually
 require_update = False
 optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, params), lr=lr)
-#optimizer = torch.optim.SGD(params[8:], lr=lr, momentum=momentum, weight_decay=weight_decay)
-optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, params), \
-                        lr=lr, momentum=momentum, weight_decay=weight_decay)
+
+### SGD with momentum ###
+#require_update = True
+#optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, params), \
+#                        lr=lr, momentum=momentum, weight_decay=weight_decay)
+##############################
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -133,7 +126,6 @@ for step in range(start_step, end_step+1):
     gt_ishard = blobs['gt_ishard']
     dontcare_areas = blobs['dontcare_areas']
     #print 'now at %s'%blobs['im_name']
-    #pdb.set_trace()
     # forward
     net(im_data, im_info, gt_boxes, gt_ishard, dontcare_areas)
     loss = net.loss + net.rpn.loss
@@ -146,14 +138,15 @@ for step in range(start_step, end_step+1):
     #print 'tp:%f, tf:%f, fg:%f, bg:%f'%(tp, tf, fg, bg)
     train_loss += loss.data[0]
     step_cnt += 1
-    #pdb.set_trace()
     # backward
     optimizer.zero_grad()
     loss.backward()
     network.clip_gradient(net, 10.)
     optimizer.step()
-    #pdb.set_trace()
-    total_loss = net.rpn.cross_entropy.data.cpu().numpy()[0]+ net.rpn.loss_box.data.cpu().numpy()[0]+net.cross_entropy.data.cpu().numpy()[0]+net.loss_box.data.cpu().numpy()[0]
+    total_loss = net.rpn.cross_entropy.data.cpu().numpy()[0]+ \
+                 net.rpn.loss_box.data.cpu().numpy()[0]+ \
+                 net.cross_entropy.data.cpu().numpy()[0]+ \
+                 net.loss_box.data.cpu().numpy()[0]
     if step % disp_interval == 0:
         duration = t.toc(average=False)
         fps = step_cnt / duration
@@ -168,7 +161,6 @@ for step in range(start_step, end_step+1):
                 net.rpn.cross_entropy.data.cpu().numpy()[0], net.rpn.loss_box.data.cpu().numpy()[0],
                 net.cross_entropy.data.cpu().numpy()[0], net.loss_box.data.cpu().numpy()[0],
                 total_loss))
-            #print 'run'
         re_cnt = True
 
     if use_tensorboard and step % log_interval == 0:
@@ -191,7 +183,7 @@ for step in range(start_step, end_step+1):
         # evaluation
         print('Now evaluating...')
         try:
-            bashCommand0 = "rm data/cache/annots.pk"
+            bashCommand0 = "rm data/cache/annots.pkl"
             process0 = subprocess.Popen(bashCommand0.split(), stdout=subprocess.PIPE)
             output, error = process0.communicate()
         except:
@@ -200,13 +192,9 @@ for step in range(start_step, end_step+1):
         bashCommand1 = "./test.py --name {} --it {:d} --rset {}".format(cut_name, step, 'test')
         process1 = subprocess.Popen(bashCommand1.split(), stdout=subprocess.PIPE)
         output, error = process1.communicate()
-    # plot loss to jpg file
-    #if (step % 500 == 0) and step > 0:
-    #    os.system('python /home/closerbibi/workspace/tools/plot_pytorch_loss.py --inputfile=/home/closerbibi/workspace/pytorch/faster-rcnn_HaoEvaluation/log/os.system("python /home/closerbibi/workspace/tools/plot_pytorch_loss.py --inputfile=/home/closerbibi/workspace/pytorch/faster-rcnn_HaoEvaluation/log/log_trying.txt')
     if step in lr_decay_steps and require_update:
         lr *= lr_decay
-        #optimizer = torch.optim.SGD(params[8:], lr=lr, momentum=momentum, weight_decay=weight_decay)
-        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, params[8:]), \
+        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, params), \
                                 lr=lr, momentum=momentum, weight_decay=weight_decay)
 
     if re_cnt:
@@ -215,3 +203,4 @@ for step in range(start_step, end_step+1):
         step_cnt = 0
         t.tic()
         re_cnt = False
+
